4f3366683d0c81c78260740b0c80e91c538b2f2b
YKarsten <yannik.karsten@web.de>
2023-05-25 09:58:46 +0200

    Setup airport model

---
+++ app/models/airport.rb
@@ -0,0 +1,6 @@
+class Airport < ApplicationRecord
+  has_many :departing_flights, foreign_key: :departure_airport, class_name: 'Flight'
+  has_many :arriving_flights, foreign_key: :arrival_airport, class_name: 'Flight'
+
+  validates :code, :name, presence: true
+end
