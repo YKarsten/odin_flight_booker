b3f08eed4c35fe5713b8332da9d78ed7309605e4
YKarsten <yannik.karsten@web.de>
2023-05-25 10:55:01 +0200

    Build #self.user_search(query_params) to match db entries against search form params

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,9 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: 'Airport'
 
   validates :departure_time, :duration, presence: true
+
+  def self.user_search(query_params)
+    where(departure_airport_id: query_params[:departure_airport_id], arrival_airport_id: query_params[:arrival_airport_id],
+    departure_date: query_params[:departure_date])
+  end
 end
