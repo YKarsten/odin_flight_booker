ad48028c995faf4c9ace59afb1c9850f00c508ff
YKarsten <yannik.karsten@web.de>
2023-05-25 12:15:56 +0200

    Add methods to format flight informations date and time

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,7 +5,15 @@ class Flight < ApplicationRecord
   validates :departure_time, :duration, presence: true
 
   def self.user_search(query_params)
-    where(departure_airport_id: query_params[:departure_airport_id], arrival_airport_id: query_params[:arrival_airport_id],
-    departure_date: query_params[:departure_date])
+    where(departure_airport_id: query_params[:departure_airport_id], arrival_airport_id: query_params[:arrival_airport_id])
+    #,departure_date: query_params[:departure_date]
+  end
+
+  def departure_date_formatted
+    departure_date.strftime("%B %d, %Y")
+  end
+
+  def departure_time_formatted
+    departure_time.strftime("%l:%M %P")
   end
 end
