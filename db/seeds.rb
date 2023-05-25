6d6bdbc7a51d3e0431d68e7497d82ae93d6e491e
YKarsten <yannik.karsten@web.de>
2023-05-25 09:55:23 +0200

    Create 10 airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.create(name: 'Bremen', code: 'BRE')
+Airport.create(name: 'Berlin Brandenburg', code: 'BER')
+Airport.create(name: 'Dortmund', code: 'DTM')
+Airport.create(name: 'Düsseldorf', code: 'DUS')
+Airport.create(name: 'Frankfurt', code: 'FRA')
+Airport.create(name: 'Hamburg', code: 'HAM')
+Airport.create(name: 'Hannover Langenhagen', code: 'HAJ')
+Airport.create(name: 'Köln/ Bonn', code: 'CGN')
+Airport.create(name: 'München', code: 'MUC')
+Airport.create(name: 'Stuttgart', code: 'STR')
+
+puts 'Created 10 airports'
