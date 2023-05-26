00c9cbcafc902c2331c9341fca49df3a750c7260
YKarsten <yannik.karsten@web.de>
2023-05-26 15:53:19 +0200

    fixed a typo

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
   def show
-    @bookign = Booking.find(params[:id])
+    @booking = Booking.find(params[:id])
   end
 
   def new
@@ -23,6 +23,6 @@ class BookingsController < ApplicationController
 
   private
   def booking_params
-    params.require(:booking).permit(:flight_id, passengers_attributes:[:name, :email])
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
