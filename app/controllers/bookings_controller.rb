15828981da1425083323d1f9cd405ba7829751be
YKarsten <yannik.karsten@web.de>
2023-05-26 15:29:05 +0200

    Add Bookings controller and #new #create #show methods

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,28 @@
+class BookingsController < ApplicationController
+  def show
+    @bookign = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @num_of_passengers = params[:num_of_passengers]
+    @booking.passengers.build
+  end
+
+  def create
+    @flight = Flight.find(booking_params[:flight_id])
+    @booking = @flight.bookings.build(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes:[:name, :email])
+  end
+end
