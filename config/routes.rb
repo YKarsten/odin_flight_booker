bd3daf5a6d737f84c089a11a50376f8634aea05c
YKarsten <yannik.karsten@web.de>
2023-05-26 15:54:37 +0200

    changed individual get lines to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'flights#index'
   resources :flights
+  resources :bookings
 end
